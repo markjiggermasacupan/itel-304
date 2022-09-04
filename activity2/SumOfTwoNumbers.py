70dd99f9d1b7712bcfab066d5abfd6ec41a454bc
Masacupan, Mark Jigger V <91792605+markjiggermasacupan@users.noreply.github.com>
2022-09-04 21:49:16 +0800

    Update SumOfTwoNumbers.py

--- activity2/SumOfTwoNumbers.py
+++ activity2/SumOfTwoNumbers.py
@@ -1,1 +1,21 @@
+import sys
 
+
+def main():
+    while True:
+        try:
+            mynum1 = float(input("Enter your first number: "))
+            mynum2 = float(input("Enter your second number: "))
+            if not (mynum1 or mynum2 < 0):
+                raise ValueError()
+
+            print(mynum1, " + ", mynum2, "is", (mynum1 + mynum2))
+            sys.exit(0)
+
+        except Exception as j:
+            print(j)
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
