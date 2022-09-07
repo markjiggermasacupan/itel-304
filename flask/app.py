cb2748872de8d94b5de3b0cc8f6903f589e645be
Masacupan, Mark Jigger V <91792605+markjiggermasacupan@users.noreply.github.com>
2022-09-07 09:28:41 +0800

    Create app.py

---
+++ flask/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+app.run('/')
+
+@app.route('/')
+
+def hello():
+    return """<html><body>
+    <h1>Hello, World!</h1>
+    The time is {0}.</body><html>""".format(str(datetime.now()))
+
+    # Launch the Flask dev server
+    app.run(host="localhost", debug=True)
+
+
+if __name__ == '__main__':
+    app.run()
