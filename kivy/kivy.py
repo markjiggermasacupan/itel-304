c96ede685ec8903701ae24ef3f8b92ca5abbc869
Masacupan, Mark Jigger V <markjiggermasacupan.ph@gmail.com>
2022-09-09 19:16:58 +0800

    Update kivy.py

--- kivy/kivy.py
+++ kivy/kivy.py
@@ -1,1 +1,31 @@
-import kivy
+from kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self)
+        return Label(text="BSIT 3RD YEAR (WMAD) AY 2022-23")
+
+MyApp().run() 
+
+if __name__ == '__main__':
+    app.run()
+
+# Process finished with exit code 0
+
+
+# Installing Kivy on Windows:
+
+#1 Ensure you have the latest pip and wheel:
+# python -n -pip install --upgrade pip wheel setuptools
+
+#2 Install the dependencies:
+# python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+# python -m pip install kivy.desp.angle
+
+#3 Install Kivy
+# python -m pip install kivy
+
+#4 As Optional, you may try Installing Kivy with examples
+# python -m pip install kivy examples
