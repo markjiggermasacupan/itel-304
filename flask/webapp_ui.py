b89c7db779382da6f0ed5517e79033b58d525b3d
Masacupan, Mark Jigger V <markjiggermasacupan.ph@gmail.com>
2022-09-09 19:05:59 +0800

    Update webapp_ui.py

--- flask/webapp_ui.py
+++ flask/webapp_ui.py
@@ -43,7 +43,6 @@ if __name__ == '__main__':
     app.run()
 
 # The program is organized as follows:
-# * Lines 6-17 define the home( ) function, which defines the starting page for the application.
-# It displays a form that prompts for the user’s name and favorite food.
+# * Lines 6-17 define the home( ) function, which defines the starting page for the application. It displays a form that prompts for the user’s name and favorite food.
 # * The form’s action attribute on Line 9 specifies that the form submission will be directed to the path /greet. Processing for this path is defined by the greet( ) function on lines 19-33.
 # * Lines 21-26 extract the information submitted on the form and compute a response message.
