33deffd102a0eaa1a1d7b6b95a15e42a6786b96f
Masacupan, Mark Jigger V <markjiggermasacupan.ph@gmail.com>
2022-09-13 16:16:24 +0800

    Update ITEL304_ACTIVITY3_MASACUPAN.py

--- ITEL304_ACTIVITY3_MASACUPAN.py
+++ ITEL304_ACTIVITY3_MASACUPAN.py
@@ -1,4 +1,6 @@
+from codecs import charmap_decode
 from flask import Flask, request
+
 from datetime import datetime
 
 app = Flask(__name__)
@@ -8,10 +10,10 @@ app = Flask(__name__)
 def home():
     return """
     <html><body>
-        <h2> Welcome Man!</h2>
+        <h4> ITEL 304 (Integrative and Programming Technologies 2) </h4>
         <form action="/greet">
             Your full name {0} <input type='text' name='fname'><br>
-            And your characteristic {0} <input type='text' name='characteristic'><br>
+            And your characteristic  {0} <input type='text' name='char'><br>
             <input type='submit' value='Continue'>
         </form>
     </body><html>
@@ -20,38 +22,26 @@ def home():
 
 @app.route("/greet")
 def greet():
+    fname = request.args.get('fname', 'Mark Jigger Masacupan')
+    char = request.args['char']
+    fixedword = ('is')
 
-    username = request.args.get('username', '')
-    characteristic = request.args['characteristic', '']
-    if username == '' and characteristic == '':
-        msg = 'Invalid input, you do not input your username and characteristic'
-
-        username = request.args.get('username', '')
-        characteristic = request.args['characteristic', '']
-    elif username == '' or characteristic == '':
-        msg = 'Invalid input, you do not input your username and characteristic'
-
-        username = request.args.get('username')
-        characteristic = request.args['characteristic', '']
-    elif characteristic == '':
-        msg = 'Your characteristic is unknown, please enter!'
-
-        username = request.args.get('username', '')
-        characteristic = request.args['characteristic']
-    elif username == '':
-        msg = 'You do not input your name, please enter!'
-
-        username = request.args.get('username')
-        characteristic = request.args['characteristic']
+    if fname == '' and char == '':
+        msg = 'You did not tell your name and characteristic'
+    elif fname == '':
+        msg = 'You did not tell your name but you are ' + char
+    elif char == '':
+        msg = 'Your name ' + fixedword + ' ' + fname + \
+            ' but you did not mention your characteristic'
     else:
-        msg = 'You are' + characteristic
+        msg = 'Your name ' + fixedword + ' ' + fname + ' and you are ' + char
 
     return """
         <html><body>
-            <h2>Hello, {0}!</h2>
-            <p> You are {1}</p>
+            <h2>Hello {0}!</h2>
+            {1}
       </body></html>
-      """.format(username, msg)
+      """.format(fname, msg)
 
 
 # Launch the FlaskPy dev server
